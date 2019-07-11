a19b2b01dd51e95569c1c286ec9da094dcc3bb3a
kimura <csma18004@g.nihon-u.ac.jp>
2019-07-11 12:28:21 +0900

    Added view and template for detailed blog post

--- blogApp1/views.py
+++ blogApp1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,7 @@ def post_list(request):
     return render(request, 'blogApp1/post_list.html', {
             'posts' : posts,
             })  #ひとつ目の引数はerquest、2つ目は表示するhtmlファイル、3つ目はhtmlを表示するときに使うデータベースの情報
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogApp1/post_detail.html', {'post': post})
